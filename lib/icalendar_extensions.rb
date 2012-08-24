d50bb0acc135d2a40303e507747772bbd7ad1539
rwlc3 <rwlc3@59899196-baf2-4bc4-a94d-4a08719e171f>
2012-08-24 15:44:10 +0000

    TLK-152 calendar feed: explicitly output London Timezone instead of wrongly saying UTC all year round
    git-svn-id: http://source.caret.cam.ac.uk/svn/projects/talks.cam/railsapp/trunk@12684 59899196-baf2-4bc4-a94d-4a08719e171f

--- lib/icalendar_extensions.rb
+++ lib/icalendar_extensions.rb
@@ -1,5 +1,5 @@
 # Specify a custom time format for icalendar
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:ics => '%Y%m%dT%H%M%SZ')
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:ics => '%Y%m%dT%H%M%S')
 
 # Specify a custom string format for icalendar
 class String
@@ -21,7 +21,25 @@ end
 class Array
   
   def to_ics
+    london_vtimezone = ("BEGIN:VTIMEZONE\r\n" +
+      "TZID:Europe/London\r\n" +
+      "BEGIN:DAYLIGHT\r\n" +
+      "TZOFFSETFROM:+0000\r\n" +
+      "TZOFFSETTO:+0100\r\n" +
+      "TZNAME:BST\r\n" +
+      "DTSTART:19700329T010000\r\n" +
+      "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n" +
+      "END:DAYLIGHT\r\n" +
+      "BEGIN:STANDARD\r\n" +
+      "TZOFFSETFROM:+0100\r\n" +
+      "TZOFFSETTO:+0000\r\n" +
+      "TZNAME:GMT\r\n" +
+      "DTSTART:19701025T020000\r\n" +
+      "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n" +
+      "END:STANDARD\r\n" +
+      "END:VTIMEZONE\r\n")
     ("BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//talks.cam.ac.uk//v3//EN\r\n" +
+    london_vtimezone +
     map {|element| element.to_ics }.join("\r\n") +
     "\r\nEND:VCALENDAR\r\n")
   end
