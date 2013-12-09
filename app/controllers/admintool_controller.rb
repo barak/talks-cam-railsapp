bf8fa3c53ba15b40e6e36915c2a79b18ff303a64
rwlc3 <rwlc3@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-12-09 16:26:42 +0000

    Tidy-up inc move non-action methods to private
    git-svn-id: http://source.caret.cam.ac.uk/svn/projects/talks.cam/railsapp/branches/admintool@22590 59899196-baf2-4bc4-a94d-4a08719e171f

--- app/controllers/admintool_controller.rb
+++ app/controllers/admintool_controller.rb
@@ -4,18 +4,6 @@ class AdmintoolController < ApplicationController
   before_filter :ensure_user_is_admin
   before_filter :find_user, :only => [:edit_crsid, :set_crsid, :delete_crsid]
 
-  def ensure_user_is_admin
-    unless User.current.administrator?
-      raise "User must be administrator"
-    end
-    # Should never get here, but return correct boolean for filter chain anyway
-    return User.current.administrator?
-  end
-
-  def find_user
-    @user = User.find params[:id]
-  end
-
   def search
     @search = params[:search]
     @users = search_all_users(@search)
@@ -29,12 +17,6 @@ class AdmintoolController < ApplicationController
     modify_crsid(nil)
   end
 
-  def search_all_users(search_term)
-    # URGH copied from User.search in models but modified...
-    return [] unless search_term && !search_term.empty?
-    User.find(:all, :conditions => ["(name LIKE :search OR affiliation LIKE :search OR email LIKE :search OR crsid LIKE :search)",{:search => "%#{search_term.strip}%"}], :order => 'email ASC' )
-  end
-
   def find_admins
     @disable_search_box = true
     @heading2text = "Administrator = true"
@@ -44,11 +26,27 @@ class AdmintoolController < ApplicationController
 
   private
 
-  # XXX this changes the email address !?!?
+  def ensure_user_is_admin
+    unless User.current.administrator?
+      raise "User must be administrator"
+    end
+    # Should never get here, but return correct boolean for filter chain anyway
+    return User.current.administrator?
+  end
+
+  def find_user
+    @user = User.find params[:id]
+  end
+
   def modify_crsid(new_crsid)
     @user.crsid = new_crsid
     @user.save!
     redirect_to :action => 'edit_crsid', :id => @user.id
+  end
+
+  def search_all_users(search_term)
+    return [] unless search_term && !search_term.empty?
+    User.find(:all, :conditions => ["(name LIKE :search OR affiliation LIKE :search OR email LIKE :search OR crsid LIKE :search)",{:search => "%#{search_term.strip}%"}], :order => 'email ASC' )
   end
 
 end
