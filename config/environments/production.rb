aede33577f6b3eccae81e3e42649bb3c4dab0921
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2014-02-05 12:04:29 +0000

    Make _session_id cookie have secure flag to avoid leaking via http
    git-svn-id: http://source.caret.cam.ac.uk/svn/projects/talks.cam/railsapp/branches/https@23075 59899196-baf2-4bc4-a94d-4a08719e171f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,3 +17,8 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Set the secure flag on cookies when sending cookies on HTTPS request responses
+# This is important to ensure a cookie transferred via HTTPs cannot be intercepted
+# by someone manually visiting or following a link to http://talks.cam
+ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS[:session_secure] = true
